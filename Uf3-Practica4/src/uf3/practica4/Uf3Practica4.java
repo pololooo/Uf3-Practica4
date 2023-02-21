65d51128985c92ff4c1fd0e23c51415022d97512
ausias <ausias@hINFO6-8.xiam.cat>
2023-02-21 13:23:42 +0100

    pol

---
+++ Uf3-Practica4/src/uf3/practica4/Uf3Practica4.java
@@ -0,0 +1,21 @@
+/*
+1. Adapta el programa que vas fer a l’exercici 3 per la gestió de clients de forma que
+faci tot el que es demanava a l’exercici 3, però a més:
+a) Accedeixi de forma directa a un registre segons la seva posició
+b) Accedeixi de forma directa a un registre segons el seu codi
+c) No hagi de reconstruir el fitxer quan s’esborra un registre
+d) No hagi de reconstruir el fitxer quan es modifica un registre
+e) Llisti els clients ordenats per codi
+Cal que adjunteu a la resolució de la pràctica una captura de pantalla on es vegi
+el contingut del fitxer en format hexadecimal de tots els fitxers que feu servir
+(índexs i dades)
+ */
+package uf3.practica4;
+
+public class Uf3Practica4 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
